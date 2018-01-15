b2e61f433485a1fa4395e62a54ec614ecbf23fc9
Sergey Pisarenko <sergey@pisarenko.net>
2018-01-15 19:51:48 +0100

    implement fill program

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,87 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+	// initialize the wanted and current colors
+	@current_color
+	M=0
+	@wanted_color
+	M=0
+
+	// compute end address of the screen
+	@SCREEN
+	D=A
+	@8192
+	D=D+A
+	@end_screen_address
+	M=D
+
+(LOOP_KEYBOARD_CHECK)
+	// wanted_color = current_color
+	@wanted_color
+	D=M
+	@current_color
+	M=D
+
+	@KBD
+	D=M
+	@SET_WANTED_COLOR_WHITE
+	D;JEQ
+
+(SET_WANTED_COLOR_BLACK)
+	// set wanted color to black
+	@wanted_color
+	M=-1
+	// if current_color == wanted_color, go to LOOP_KEYBOARD_CHECK
+	D=M
+	@current_color
+	D=D-M
+	@LOOP_KEYBOARD_CHECK
+	D;JEQ
+	// screen repaint is necessary
+	@SCREEN_PAINT
+	0;JMP
+
+(SET_WANTED_COLOR_WHITE)
+	// set wanted color to white
+	@wanted_color
+	M=0
+	// if current_color == wanted_color, go to LOOP_KEYBOARD_CHECK
+	D=M
+	@current_color
+	D=D-M
+	@LOOP_KEYBOARD_CHECK
+	D;JEQ
+	// screen repaint is necessary
+
+(SCREEN_PAINT)
+	// initialize screen paint counter
+	@SCREEN
+	D=A
+	@curr_screen_address
+	M=D
+
+(LOOP_SCREEN_PAINT)
+	// if curr_screen_address == end_screen_address
+	@curr_screen_address
+	D=M
+	@end_screen_address
+	D=M-D
+	@LOOP_KEYBOARD_CHECK
+	D;JEQ
+
+    // paint one word at a time
+	@wanted_color
+	D=M
+	@curr_screen_address
+	A=M
+	M=D
+
+	// curr_screen_address += word
+	@curr_screen_address
+	D=M
+	D=D+1
+	M=D
+
+	@LOOP_SCREEN_PAINT
+	0;JMP
