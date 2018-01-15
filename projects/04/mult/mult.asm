54d493959a197f6e00cc544fd6554bb4f2ce2f93
Sergey Pisarenko <sergey@pisarenko.net>
2018-01-15 12:51:33 +0100

    implement trivial multiplication using add instruction

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,32 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+	@n
+	M=0
+	@R2
+	M=0
+
+(LOOP)
+	@n
+	D=M
+
+	@R0    // if n=R1, end loop
+	D=D-M
+	@END
+	D;JEQ
+
+	@n
+	D=M
+	M=D+1  // n++
+
+	@R1
+	D=M
+	@R2
+	M=M+D  // R2 += R2
+
+	@LOOP
+	0;JMP  // continue looping
+
+(END)
+    @END
+	0;JMP
